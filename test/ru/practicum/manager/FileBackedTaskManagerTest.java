4ab18342347fe76a844e41a30da57e48ddc83249
Mariia Postnova <dev.nuggets@yandex.com>
2025-11-01 11:10:20 +0200

    code review changes

--- test/ru/practicum/manager/FileBackedTaskManagerTest.java
+++ test/ru/practicum/manager/FileBackedTaskManagerTest.java
@@ -11,7 +11,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.stream.Collectors;
 
 
@@ -36,26 +35,9 @@ class FileBackedTaskManagerTest {
     void when_addsDifferentTasksToManagerAndOtherManagerLoadsFromTheSameFile_should_beEqual() throws IOException {
         // given
         var file = File.createTempFile("test", "FileBackedTaskManager");
-        var originalManager = new FileBackedTaskManager(file);
-        var task1 = new Task("Задача 1", "Описание задачи 1", 0, TaskStatus.NEW);
-        var task2 = new Task("Задача 2", "Описание задачи 2", 0, TaskStatus.NEW);
-        var epic1 = new Epic("Эпик 1", "Описание эпика 1", 0, TaskStatus.NEW);
-        var epic2 = new Epic("Эпик 2", "Описание эпика 2", 0, TaskStatus.NEW);
 
-        //do
-        originalManager.createTask(task1);
-        originalManager.createTask(task2);
-        var epic1Id = originalManager.createEpic(epic1);
-        var epic2Id = originalManager.createEpic(epic2);
-        var subtask1 = new Subtask("Подзадача первого эпика 1", "Описание подзадачи 1", 0,
-                TaskStatus.NEW, epic1Id);
-        var subtask2 = new Subtask("Подзадача первого эпика 2", "Описание подзадачи 2", 0,
-                TaskStatus.NEW, epic1Id);
-        var subtask3 = new Subtask("Подзадача второго эпика", "Описание подзадачи 1", 0,
-                TaskStatus.NEW, epic2Id);
-        originalManager.createSubtask(subtask1);
-        originalManager.createSubtask(subtask2);
-        originalManager.createSubtask(subtask3);
+        // do
+        var originalManager = getFileBackedTaskManager(file);
         var managerFromFile = FileBackedTaskManager.loadFromFile(file);
 
         // expect
@@ -82,6 +64,30 @@ class FileBackedTaskManagerTest {
         Assertions.assertEquals(originalManager.epics.size(), managerFromFile.epics.size());
         Assertions.assertEquals(originalManager.subtasks.size(), managerFromFile.subtasks.size());
         Assertions.assertEquals(originalManager.uniqueTaskId, managerFromFile.uniqueTaskId);
+    }
+
+    private FileBackedTaskManager getFileBackedTaskManager(File file) {
+        var originalManager = new FileBackedTaskManager(file);
+        var task1 = new Task("Задача 1", "Описание задачи 1", 0, TaskStatus.NEW);
+        var task2 = new Task("Задача 2", "Описание задачи 2", 0, TaskStatus.NEW);
+        var epic1 = new Epic("Эпик 1", "Описание эпика 1", 0, TaskStatus.NEW);
+        var epic2 = new Epic("Эпик 2", "Описание эпика 2", 0, TaskStatus.NEW);
+
+        //do
+        originalManager.createTask(task1);
+        originalManager.createTask(task2);
+        var epic1Id = originalManager.createEpic(epic1);
+        var epic2Id = originalManager.createEpic(epic2);
+        var subtask1 = new Subtask("Подзадача первого эпика 1", "Описание подзадачи 1", 0,
+                TaskStatus.NEW, epic1Id);
+        var subtask2 = new Subtask("Подзадача первого эпика 2", "Описание подзадачи 2", 0,
+                TaskStatus.NEW, epic1Id);
+        var subtask3 = new Subtask("Подзадача второго эпика", "Описание подзадачи 1", 0,
+                TaskStatus.NEW, epic2Id);
+        originalManager.createSubtask(subtask1);
+        originalManager.createSubtask(subtask2);
+        originalManager.createSubtask(subtask3);
+        return originalManager;
     }
 
     private void assertTasksAreEqual(Task left, Task right) {
